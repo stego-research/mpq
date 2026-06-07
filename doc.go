e3c4fe70ec022e45427d3813926e249acc45424c
Brendan Dibbell <stego@stego.ai>
2026-06-07 15:20:50 -0400

    go fmt

--- doc.go
+++ doc.go
@@ -1,11 +1,10 @@
 /*
-
 Package mpq is a decoder/parser of Blizzard's MPQ archive file format.
 
 This is not a full MPQ implementation. It is primarily intended to parse StarCraft II replay files (*.SC2Replay),
 but that is fully supported.
 
-Usage
+# Usage
 
 Usage is simple. Opening an MPQ archive file:
 
@@ -32,8 +31,7 @@ If you already have the MPQ data in memory:
 	mpqdata := []byte{} // MPQ data in memory
 	m, err := mpq.New(bytes.NewReader(mpqdata)))
 
-
-Information sources
+# Information sources
 
 The_MoPaQ_Archive_Format: http://wiki.devklog.net/index.php?title=The_MoPaQ_Archive_Format
 
@@ -50,13 +48,13 @@ MPQ parser of the Scelight project: https://github.com/icza/scelight/tree/master
 Format of the "(attributes)" meta attributes file:
 https://github.com/stormlib/StormLib/blob/3a926f0228c68d7d91cf3946624d7859976440ec/src/SFileAttributes.cpp
 
-    int version: Version of the (attributes) file. Must be 100 (0x64)
-    int flags: flags telling what is contained in the "(attributes)"
-        MPQ_ATTRIBUTE_CRC32         0x00000001  The "(attributes)" contains CRC32 for each file
-        MPQ_ATTRIBUTE_FILETIME      0x00000002  The "(attributes)" contains file time for each file
-        MPQ_ATTRIBUTE_MD5           0x00000004  The "(attributes)" contains MD5 for each file
-        MPQ_ATTRIBUTE_PATCH_BIT     0x00000008  The "(attributes)" contains a patch bit for each file
-        MPQ_ATTRIBUTE_ALL           0x0000000F  Summary mask
+	int version: Version of the (attributes) file. Must be 100 (0x64)
+	int flags: flags telling what is contained in the "(attributes)"
+	    MPQ_ATTRIBUTE_CRC32         0x00000001  The "(attributes)" contains CRC32 for each file
+	    MPQ_ATTRIBUTE_FILETIME      0x00000002  The "(attributes)" contains file time for each file
+	    MPQ_ATTRIBUTE_MD5           0x00000004  The "(attributes)" contains MD5 for each file
+	    MPQ_ATTRIBUTE_PATCH_BIT     0x00000008  The "(attributes)" contains a patch bit for each file
+	    MPQ_ATTRIBUTE_ALL           0x0000000F  Summary mask
 
 If has CRC32: int * BlockTableSize
 
@@ -65,6 +63,5 @@ If has FILETIME: long * BlockTableSize
 If has MD5: MD5SIZE * BlockTableSize
 
 If has PATCH_BIT: enough bytes to hold BlockTableSize bits
-
 */
 package mpq
